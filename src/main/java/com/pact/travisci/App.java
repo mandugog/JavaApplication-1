396ed5a14023762b4c142f5a5ec151110144a8d5
mandugog <mgogate@centtra.com>
2015-11-25 14:26:08 +0530

    added files

--- src/main/java/com/pact/travisci/App.java
+++ src/main/java/com/pact/travisci/App.java
@@ -1,51 +1,44 @@
-package com.pact.travisci;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-
 /**
  * Author: Mukta Aphale
- *
+ * Reader: DevOps Trainee
  */
 public class App {
-	private static URLConnection connect(String urlString) {
-		try {
-			URL url = new URL(urlString);
-			URLConnection con = url.openConnection();
-			return con;
-		}
-		catch (Exception e) {
-			System.err.println(e.getMessage());
-			return null;
-		}
-	}
+        private static URLConnection connect(String urlString) {
+                try {
+                        URL url = new URL(urlString);
+                        URLConnection con = url.openConnection();
+                        return con;
+                }
+                catch (Exception e) {
+                        System.err.println(e.getMessage());
+                        return null;
+                }
+        }
 
-	private static String readPage(URLConnection con){
-		String pageSource = "";
-		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-			String curLine;
-			StringBuilder builder = new StringBuilder();
-			while((curLine = br.readLine()) != null) {
-				builder.append(curLine);
-			}
-			br.close();
-			pageSource = builder.toString();
-		} catch (Exception e) {
-			System.err.println(e.getMessage());
-		}
-		return pageSource;
-	}
+        private static String readPage(URLConnection con){
+                String pageSource = "";
+                try {
+                        BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                        String curLine;
+                        StringBuilder builder = new StringBuilder();
+                        while((curLine = br.readLine()) != null) {
+                                builder.append(curLine);
+                        }
+                        br.close();
+                        pageSource = builder.toString();
+                } catch (Exception e) {
+                        System.err.println(e.getMessage());
+                }
+                return pageSource;
+        }
 
-	public static String getPageSource(String urlString) {
-		URLConnection con = connect(urlString);
-		String pageSource = readPage(con);
-		return pageSource;
-	}
+        public static String getPageSource(String urlString) {
+                URLConnection con = connect(urlString);
+                String pageSource = readPage(con);
+                return pageSource;
+        }
 
-	public static void main(String[] args) {
-		System.out.println(getPageSource(args[0]));
-	}
+        public static void main(String[] args) {
+                System.out.println(getPageSource(args[0]));
+        }
 }
